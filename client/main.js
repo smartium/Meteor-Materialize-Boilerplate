2afcb016d36eb29fe884c6cc0bd9042aaaf26a1b
SMARTIUM <developer@smartium.app>
2020-04-15 13:54:54 -0300

    Add social icons theme by https://terrymooreii.github.io/materialize-social/

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import '../imports/client/lib/routes';
 import '../node_modules/materialize-css/sass/materialize.scss';
+import '../imports/client/styles/social.css';
 import '../node_modules/materialize-css/dist/js/materialize.min';
 import '../imports/client/partials/header/header';
 import '../imports/client/partials/footer/footer';
